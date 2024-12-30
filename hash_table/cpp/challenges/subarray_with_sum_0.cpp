fea219fe157cc144e53c011a0ae50a350cff3c6d
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-12-31 00:01:03 +0700

    hash-table: solve challenge of find sub-array with sum 0 in c++

--- hash_table/cpp/challenges/subarray_with_sum_0.cpp
+++ hash_table/cpp/challenges/subarray_with_sum_0.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-bool FindSubZero(int *arr, int size)
+bool FindSubZero_S1(int *arr, int size)
 {
     unordered_map<int, bool> sumMap;
 
@@ -19,4 +19,22 @@ bool FindSubZero(int *arr, int size)
     }
 
     return sumMap.find(0) != sumMap.end();
+}
+
+bool FindSubZero_S2(int *arr, int size)
+{
+    for (auto i = 0; i < size; i++)
+    {
+        for (auto j = i; j < size; j++)
+        {
+            int sum = 0;
+            for (auto k = i; k <= j; k++)
+                sum += arr[k];
+
+            if (sum == 0)
+                return true;
+        }
+    }
+
+    return false;
 }
