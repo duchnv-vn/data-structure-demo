3602d7a8216cecf23834a78ad5d4319ac2356fa3
duchnv-vn <huynhnguyenvietduc@gmail.com>
2025-01-19 13:12:23 +0700

    ptit(ctdlgt): random numbers combination generation algorithm

--- ptit/class1-data-structure-and-algorithm-basics/homework/problem_8.cpp
+++ ptit/class1-data-structure-and-algorithm-basics/homework/problem_8.cpp
@@ -5,47 +5,62 @@
  * Thời gian thực hiện: T(n) = O(n)
  *
  * Thời gian chạy chương trình thực tế:
- * 1000 numbers -> 70 ms
- * 10000 numbers -> 75 ms
- * 100000 numbers -> 80 ms
- * 1000000 numbers -> 100 ms
+ * 1000 numbers -> ~127 ms
+ * 10000 numbers -> ~140 ms
+ * 100000 numbers -> ~145 ms
+ * 1000000 numbers -> ~150 ms
  */
 
-#include <algorithm>
 #include <iostream>
 #include <chrono>
-#include <random>
 using namespace std;
 
-void generateRandomNum(int *arr, const int n) {
-    const auto minValue = 1;
-    const auto maxValue = 1000000;
-    const auto randomNums = new int [maxValue + 1];
-
-    for (int i = minValue; i <= 1000000; i++) {
-        randomNums[i] = i;
-    }
-
-    for (int i = 0; i < n; i++) {
-        arr[i] = randomNums[i];
-    }
-
-    delete [] randomNums;
-}
-
-int main() {
-    const auto m = 100000;
-    const auto arr = new int [m];
+void generate_random_number_combination() {
+    int m;
+    cout << "Nhap m so ngau nhien muon lay ra: ";
+    cin >> m;
 
     const auto beg = chrono::high_resolution_clock::now();
 
-    generateRandomNum(arr, m);
+    const auto max = 1000000;
+    const auto randomNums = new int[max + 1];
+
+    // Sinh day so 0 - 1000000
+    for (int i = 0; i <= max; i++) randomNums[i] = i;
+
+    // Sap xep thu tu ngau nhien
+    for (int i = 0; i <= max; i++) {
+        srand(chrono::duration_cast<chrono::nanoseconds>(chrono::system_clock::now().time_since_epoch()).count());
+        const int randIdx = i + (rand() % (max - i + 1)); // sinh so ngau nhien trong khoang [i, max - i]
+        const int temp = randomNums[i];
+        randomNums[i] = randomNums[randIdx];
+        randomNums[randIdx] = temp;
+    }
+
+    // Lay ra m so ngau nhien
+    const auto arr = new int[m];
+    for (int i = 0; i < m; i++) {
+        arr[i] = randomNums[i];
+    }
 
     const auto end = chrono::high_resolution_clock::now();
     const auto duration = chrono::duration_cast<chrono::milliseconds>(end - beg);
-    cout << "Elapsed(ms) = " << duration.count() << endl;
-    cout << "First number: " << arr[0] << endl;
-    cout << "Last number: " << arr[m - 1] << endl;
+    cout << "Tong thoi gian thuc hien (ms) = " << duration.count() << endl;
+
+    for (int i = 0; i < m; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     delete[] arr;
+    delete[] randomNums;
+}
+
+int main() {
+    int test;
+    cout << "Nhap so lan chay: ";
+    cin >> test;
+    while (test--) {
+        generate_random_number_combination();
+    }
 }
