451b7a2d33f4b667b63d8e8a99cc9d087f578596
duchnv-vn <huynhnguyenvietduc@gmail.com>
2025-03-31 23:01:14 +0700

    ptit(ctdlgt): branch and bound algorithm coding

---
+++ ptit/section-3-backtracking-and-branch-bound/branch-and-bound.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int value;
+    Node *left;
+    Node *right;
+
+    explicit Node(const int v): value{v}, left{nullptr}, right{nullptr} {
+    }
+};
+
+class BinaryTree {};
+
+void leastCostSearch() {
+}
+
+// Queue model - FIFO
+void breadthFirstSearch() {
+}
+
+// Stack model - LIFO
+void depthFirstSearch() {
+}
+
+void knapsackProblem() {
+    const int N = 5, W = 10;
+    int v[N] = {40, 50, 100, 95, 30};
+    float w[N] = {2, 3.14, 1.98, 5, 3};
+}
+
+void insertNode(Node *(&node_ptr), const int v) {
+    if (node_ptr == nullptr) {
+        node_ptr = new Node{v};
+    } else {
+        node_ptr->value > v
+            ? insertNode(node_ptr->left, v)
+            : insertNode(node_ptr->right, v);
+    }
+}
+
+void createBinaryTree(Node *(&root_ptr)) {
+    const int N = 5;
+    int values[N] = {40, 50, 100, 95, 30};
+
+    for (int i = 0; i < N; i++) {
+        insertNode(root_ptr, values[i]);
+    }
+
+
+}
+
+int main() {
+    Node *root_ptr;
+    createBinaryTree(root_ptr);
+    return 0;
+}
